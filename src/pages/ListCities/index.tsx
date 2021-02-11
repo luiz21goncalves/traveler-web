d0fc1010ee93c610ac7bfd808bd094a091d3ba6a
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-02-10 21:22:58 -0300

    add ListCities

--- src/pages/ListCities/index.tsx
+++ src/pages/ListCities/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { FiChevronDown } from 'react-icons/fi';
 
+import image1 from '../../assets/image-1.png';
+import image2 from '../../assets/image-2.png';
+import image3 from '../../assets/image-3.png';
+import image4 from '../../assets/image-4.png';
+import image5 from '../../assets/image-5.png';
+import image6 from '../../assets/image-6.png';
+import image7 from '../../assets/image-7.png';
+import image8 from '../../assets/image-8.png';
+import Card from '../../components/Card';
 import Header from '../../components/Header';
 
 import { Container } from './styles';
@@ -8,7 +18,40 @@ const ListCities: React.FC = () => {
   return (
     <Container>
       <Header />
-      <h1>ListCities</h1>
+
+      <main>
+        <div>
+          <h1>Selecione uma cidade</h1>
+
+          <div>
+            <button type="button">Todos</button>
+            <button type="button">Mais acessadas</button>
+            <button type="button">
+              A - Z
+              <FiChevronDown size={10} color="#F25D27" />
+            </button>
+          </div>
+        </div>
+
+        <section>
+          <Card image={image1} city="Águas Mornas" numberPlaces={13} />
+          <Card image={image2} city="Bombinhas" numberPlaces={43} />
+          <Card image={image3} city="Blumenau" numberPlaces={29} />
+          <Card image={image4} city="Florianópolis" numberPlaces={98} />
+          <Card image={image5} city="Imbituba" numberPlaces={61} />
+          <Card image={image6} city="Jaraguá do Sul" numberPlaces={41} />
+          <Card image={image7} city="Lages" numberPlaces={19} />
+          <Card image={image8} city="Rio do Sul" numberPlaces={27} />
+          <Card image={image1} city="Águas Mornas" numberPlaces={13} />
+          <Card image={image2} city="Bombinhas" numberPlaces={43} />
+          <Card image={image3} city="Blumenau" numberPlaces={29} />
+          <Card image={image4} city="Florianópolis" numberPlaces={98} />
+          <Card image={image5} city="Imbituba" numberPlaces={61} />
+          <Card image={image6} city="Jaraguá do Sul" numberPlaces={41} />
+          <Card image={image7} city="Lages" numberPlaces={19} />
+          <Card image={image8} city="Rio do Sul" numberPlaces={27} />
+        </section>
+      </main>
     </Container>
   );
 };
--- src/pages/ListCities/styles.ts
+++ src/pages/ListCities/styles.ts
@@ -1,3 +1,62 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+  main {
+    padding: 40px 160px;
+
+    div:first-child {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      h1 {
+        color: #123952;
+        font-size: 36px;
+        line-height: 46px;
+        font-weight: 600;
+      }
+
+      div {
+        width: 274;
+        height: 36px;
+        border-bottom: 1px solid #dce2e6;
+
+        button {
+          height: 100%;
+          background: transparent;
+          border: none;
+          font-family: 'Roboto';
+          color: #a0acb2;
+          padding-bottom: 10px;
+          font-weight: 400;
+
+          &:first-child {
+            font-weight: 700;
+            color: #123952;
+            border-bottom: 2px solid #f25d27;
+          }
+
+          svg {
+            margin-left: 10px;
+          }
+
+          & + button {
+            margin-left: 32px;
+          }
+        }
+      }
+    }
+
+    section {
+      display: grid;
+      grid-template-columns: repeat(5, auto);
+      justify-content: space-between;
+      gap: 32px;
+      margin-top: 39px;
+    }
+  }
+`;
